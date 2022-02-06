a0ecc842f8cd48162cd756a02cdbc511154c5929
rahmat.susanto <rahmat.susanto@ist.id>
2022-02-06 20:45:00 +0700

    structure

--- lib/presentation/pages/search_page.dart
+++ lib/presentation/pages/search_page.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:nonton_app/common/constants.dart';
+import 'package:nonton_app/common/drawer_item_enum.dart';
 import 'package:nonton_app/common/state_enum.dart';
-import 'package:nonton_app/presentation/providers/tvshow/tv_show_search_notifier.dart';
-import 'package:nonton_app/presentation/widgets/tv_show_card.dart';
+import 'package:nonton_app/presentation/providers/search_notifier.dart';
+import 'package:nonton_app/presentation/widgets/movie_card_list.dart';
 import 'package:provider/provider.dart';
 
-class SearchTvShowsPage extends StatelessWidget {
+class SearchPage extends StatelessWidget {
   static const ROUTE_NAME = '/search';
+
+  SearchPage({
+    Key? key,
+    required this.drawerItem,
+  }) : super(key: key);
+
+  final DrawerItem drawerItem;
+  late bool _isAlreadySearched = false;
+  late String _title;
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<SearchNotifier>(context);
+    _title = drawerItem == DrawerItem.Movie ? "Movie" : "TV Show";
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Search'),
@@ -21,8 +34,8 @@ class SearchTvShowsPage extends StatelessWidget {
           children: [
             TextField(
               onSubmitted: (query) {
-                Provider.of<TvShowSearchNotifier>(context, listen: false)
-                    .fetchTvShowSearch(query);
+                Provider.of<SearchNotifier>(context, listen: false)
+                    .fetchMovieSearch(query);
               },
               decoration: InputDecoration(
                 hintText: 'Search title',
@@ -36,7 +49,7 @@ class SearchTvShowsPage extends StatelessWidget {
               'Search Result',
               style: kHeading6,
             ),
-            Consumer<TvShowSearchNotifier>(
+            Consumer<SearchNotifier>(
               builder: (context, data, child) {
                 if (data.state == RequestState.loading) {
                   return Center(
@@ -48,8 +61,8 @@ class SearchTvShowsPage extends StatelessWidget {
                     child: ListView.builder(
                       padding: const EdgeInsets.all(8),
                       itemBuilder: (context, index) {
-                        final tv = data.searchResult[index];
-                        return TvShowCard(tv);
+                        final movie = data.searchResult[index];
+                        return MovieCard(movie);
                       },
                       itemCount: result.length,
                     ),
